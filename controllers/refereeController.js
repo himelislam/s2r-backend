85a15fee908ef5c78046a11542806555741ee510
Himel Islam <himel7100@gmail.com>
2025-04-18 16:54:36 +0600

    feat: payout active referee list added

--- controllers/refereeController.js
+++ controllers/refereeController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler');
 const Referrer = require('../models/referrerModel')
 const Referee = require('../models/refereeModel')
+const Campaign = require('../models/campaignModel')
 const { mailer: { client_url } } = require('../config/env')
 
 const createReferee = asyncHandler(async (req, res) => {
     const { name, email, phone, date, businessId, campaignId, referrerId } = req.body;
     try {
-        if (!name || !email || !businessId || !referrerId ) {
+        if (!name || !email || !businessId || !referrerId) {
             res.status(400).json({ message: "Please include all fields" });
             throw new Error("Please include all fields");
         }
@@ -103,10 +104,60 @@ const updateRefereeStatus = asyncHandler(async (req, res) => {
     }
 })
 
+const getRefereeWithCampaignDetails = asyncHandler(async (req, res) => {
+    const { businessId } = req.body;
+
+    try {
+        // Get referees
+        const referees = await Referee.find({ businessId, status: 'Active' });
+
+        // Get all unique campaign and referrer IDs
+        const campaignIds = [...new Set(referees.map(r => r.campaignId))];
+        const referrerIds = [...new Set(referees.map(r => r.referrerId))];
+
+        // Fetch all campaigns and referrers in parallel
+        // const [campaigns, referrers] = await Promise.all([
+        //     Campaign.find({ _id: { $in: campaignIds } }),
+        //     Referrer.find({ _id: { $in: referrerIds } })
+        // ]);
+
+        // Map data to referees
+        // const result = referees.map(referee => ({
+        //     ...referee.toObject(),
+        //     campaign: campaigns.find(c => c._id.equals(referee.campaignId)),
+        //     referrer: referrers.find(r => r._id.equals(referee.referrerId))
+        // }));
+
+
+        const detailedReferees = await Promise.all(
+            referees.map(async (ref) => {
+              const campaign = await Campaign.findById(ref.campaignId);
+              const referrer = await Referrer.findById(ref.referrerId);
+        
+              return {
+                ...ref.toObject(),
+                campaignName: campaign?.campaignName,
+                reward: campaign?.reward,
+                referrerName: referrer?.name,
+                referrerEmail: referrer?.email,
+                qrCodeId: referrer?.qrCodeId,
+                // Add anything else you need
+              };
+            })
+          );
+
+        res.json(detailedReferees);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+})
+
 module.exports = {
     createReferee,
     getRefereeByReferrerId,
     getRefereeByBusinessId,
     getRefereeList,
-    updateRefereeStatus
+    updateRefereeStatus,
+    getRefereeWithCampaignDetails
 };
--- routes/refereeRoutes.js
+++ routes/refereeRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createReferee, getRefereeByBusinessId, getRefereeByReferrerId, getRefereeList, updateRefereeStatus } = require('../controllers/refereeController')
+const { createReferee, getRefereeByBusinessId, getRefereeByReferrerId, getRefereeList, updateRefereeStatus, getRefereeWithCampaignDetails } = require('../controllers/refereeController')
 const { authMiddleware } = require('../middlewares/authMiddleware')
 
 router.post('/createReferee', createReferee)
 router.post('/getRefereeList', getRefereeList )
 router.post('/getRefereeByReferrerId', authMiddleware, getRefereeByReferrerId)
 router.post('/getRefereeByBusinessId', authMiddleware, getRefereeByBusinessId);
-router.post('/updateRefereeStatus', authMiddleware, updateRefereeStatus)
+router.post('/updateRefereeStatus', authMiddleware, updateRefereeStatus);
+router.post('/getRefereeWithCampaignDetails', authMiddleware, getRefereeWithCampaignDetails);
 
 module.exports = router;
