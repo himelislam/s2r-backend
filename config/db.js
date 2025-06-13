ab55dc868185ae49fe4e0264a3640bd3d340b5f2
Himel Islam <himel7100@gmail.com>
2025-06-13 17:24:13 +0600

    hotfix: db updated

--- config/db.js
+++ config/db.js
@@ -30,30 +30,49 @@ const { mongo: { uri } } = require("./env");
 const StripeService = require('../service/stripeService');
 const Config = require('../models/configModel');
 
+// const connectToDatabase = async () => {
+//   try {
+//     const db = await mongoose.connect(uri, {
+//       // useNewUrlParser: true,
+//       // useUnifiedTopology: true,
+//       // serverSelectionTimeoutMS: 5000,
+//     });
+//     console.log(`MongoDB Connected: ${db.connection.host}`);
+
+//     // const initialized = await Config.findOne({ key: 'stripe_initialized' });
+
+//     // if (!initialized) {
+//     //   await StripeService.initializePlans();
+//     //   console.log('Stripe plans initialized successfully');
+
+//     //   await Config.create({ key: 'stripe_initialized', value: true });
+//     // } else {
+//     //   console.log('Stripe plans already initialized, skipping...');
+//     // }
+
+//   } catch (error) {
+//     console.error(`Error connecting to database: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+
+
+
 const connectToDatabase = async () => {
   try {
     const db = await mongoose.connect(uri, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
-      // serverSelectionTimeoutMS: 5000,
     });
     console.log(`MongoDB Connected: ${db.connection.host}`);
-
-    // const initialized = await Config.findOne({ key: 'stripe_initialized' });
-
-    // if (!initialized) {
-    //   await StripeService.initializePlans();
-    //   console.log('Stripe plans initialized successfully');
-
-    //   await Config.create({ key: 'stripe_initialized', value: true });
-    // } else {
-    //   console.log('Stripe plans already initialized, skipping...');
-    // }
-
   } catch (error) {
-    console.error(`Error connecting to database: ${error.message}`);
+    console.error(`${error.message}`);
     process.exit(1);
   }
 };
 
+
 module.exports = { connectToDatabase };
+
+
