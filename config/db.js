6ab197a10ddb9d89cc59b4d7687f970001b21c57
Himel Islam <himel7100@gmail.com>
2025-06-13 16:49:11 +0600

    hotfix: updated db.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,57 @@
+// const mongoose = require("mongoose");
+// const { mongo: { uri } } = require("./env");
+// const StripeService = require('../service/stripeService');
+
+
+// const connectToDatabase = async () => {
+//   try {
+//     const db = await mongoose.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000
+//     })
+//     console.log(`MongoDB Connected: ${db.connection.host}`);
+
+//     await StripeService.initializePlans();
+//     console.log('Stripe plans initialized successfully');
+
+//     // process.exit(0);
+//   } catch (error) {
+//     console.error(`${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = { connectToDatabase };
+
+
 const mongoose = require("mongoose");
 const { mongo: { uri } } = require("./env");
 const StripeService = require('../service/stripeService');
-
+const Config = require('../models/configModel');
 
 const connectToDatabase = async () => {
   try {
     const db = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000
-    })
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log(`MongoDB Connected: ${db.connection.host}`);
 
-    await StripeService.initializePlans();
-    console.log('Stripe plans initialized successfully');
+    const initialized = await Config.findOne({ key: 'stripe_initialized' });
 
-    // process.exit(0);
+    if (!initialized) {
+      await StripeService.initializePlans();
+      console.log('Stripe plans initialized successfully');
+
+      await Config.create({ key: 'stripe_initialized', value: true });
+    } else {
+      console.log('Stripe plans already initialized, skipping...');
+    }
+
   } catch (error) {
-    console.error(`${error.message}`);
+    console.error(`Error connecting to database: ${error.message}`);
     process.exit(1);
   }
 };
